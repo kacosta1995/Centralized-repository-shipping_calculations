8f13934abfe5343031c7575908c3bb2362f5bdcd
kacosta1995 <kacosta1995@gmail.com>
2025-04-21 20:57:58 -0400

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,8 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by kacosta1995
+git config --global user.email kacosta1995@gmail.com
+git config --global user.name kacosta1995
+git add .
+git commit -m "added a new line to Shipping_Cost_Calculator.py"
+git status
